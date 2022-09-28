e32f3875ce83972edac75b7f930b7282e076c11b
Derek Colley <derek@colley.cc>
2022-09-28 09:51:18 +0100

    deploy to gh-pages

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,12 @@
 const { defineConfig } = require('@vue/cli-service')
+// import { defineConfig } from '@vue/cli-service'
+
 module.exports = defineConfig({
+
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/dot-clock/'
+    : '/',
+
   transpileDependencies: [
     'vuetify'
   ]
